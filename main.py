efb8f85b5a30c87006c52ddde7a7bbf410989f43
SenneRosaer <s0172200@ad.ua.ac.be>
2020-04-24 15:36:44 +0200

    first dataframe with dna and time of extraction

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import csv
 
-if __name__ == '__main__':
+def create_dataframe():
     pd.options.display.max_rows
     pd.set_option('display.max_rows', 500)
     pd.set_option('display.max_columns', 500)
@@ -15,9 +15,9 @@ if __name__ == '__main__':
     result = []
     for line in file:
         if "complete genome" in line:
-            line = line.replace("complete genome","")
+            line = line.replace("complete genome", "").replace(" |", "|")
             tmp = line.split("|")
-            tmp[2] = tmp[2].replace("\n","")
+            tmp[2] = tmp[2].replace("\n", "")
             result.append(tmp)
 
     df2 = pd.DataFrame(result, columns=["Accession", "extra_info", "DNA"])
@@ -26,5 +26,21 @@ if __name__ == '__main__':
     df['Accession'] = df['Accession'].astype(str)
     df2['Accession'] = df2['Accession'].astype(str)
 
-    # df = pd.merge(df, df2,  on="Accession")
-    print(df.)
+    df = pd.merge(df, df2, how="right", on="Accession")
+    del df["extra_info"]
+    del df["Publications"]
+    del df["Authors"]
+    del df["Genotype"]
+    del df["Segment"]
+    del df["Species"]
+    del df["Genus"]
+    del df["Family"]
+    del df["BioSample"]
+    del df["GenBank_Title"]
+    del df["Isolation_Source"]
+    del df["Nuc_Completeness"]
+    return df
+
+if __name__ == '__main__':
+    df = create_dataframe()
+    print(df)
